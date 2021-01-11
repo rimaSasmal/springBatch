43f6dc68bfaf3c65ddda6d65e6cc809508e411ba
rimaSasmal <rimasasmal12@gmail.com>
2021-01-11 16:20:00 +0530

    project added

---
+++ src/main/java/com/example/sprigBatch/SprigBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprigBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprigBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sprigBatch/SprigBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprigBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprigBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
